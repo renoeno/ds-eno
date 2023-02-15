5a06ea8ef2d6c2ccf6c5bd5bad8c3ed61310baf9
renoeno <reno.less@gmail.com>
2023-02-15 12:03:30 -0300

    fix: change storybook production url

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if(configType === 'PRODUCTION') {
-      config.base = '/ignite-ui-ds/'
+      config.base = '/ds-eno/'
     }
 
     return config
