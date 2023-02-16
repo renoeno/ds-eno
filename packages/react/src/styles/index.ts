c36a4d86e562d50261366f13dbeca059a443909c
renoeno <reno.less@gmail.com>
2023-02-16 12:46:39 -0300

    chore: add custom fonts

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -37,3 +37,40 @@ export const {
     transitions,
   },
 })
+
+export const globalStyles = globalCss({
+  '@font-face': [
+    {
+      fontFamily: "Oswald",
+      fontWeight: 400,
+      src: 'local("Oswald"), url(/fonts/Oswald-Regular.woff2)',
+    },
+    {
+      fontFamily: "Oswald",
+      fontWeight: 500,
+      src: 'local("Oswald"), url(/fonts/Oswald-Medium.woff2)',
+    },
+    {
+      fontFamily: "Oswald",
+      fontWeight: 700,
+      src: 'local("Oswald"), url(/fonts/Oswald-Bold.woff2)',
+    },
+    {
+      fontFamily: "Barlow",
+      fontWeight: 400,
+      src: 'local("Barlow"), url(/fonts/Barlow-Regular.ttf)',
+    },
+    {
+      fontFamily: "Barlow",
+      fontWeight: 500,
+      src: 'local("Barlow"), url(/fonts/Barlow-Medium.ttf)',
+    },
+    {
+      fontFamily: "Barlow",
+      fontWeight: 700,
+      src: 'local("Barlow"), url(/fonts/Barlow-Bold.ttf)',
+    },
+  ],
+});
+
+globalStyles()
